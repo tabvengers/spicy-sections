cb35d6d143c0c7bc884c3e7db11fbd72e335d71c
Jonathan Neal <jonathantneal@hotmail.com>
2022-03-15 15:01:10 -0400

    Add HTMLSlotElement.prototype.slot polyfill

---
+++ src/polyfill.js
@@ -0,0 +1,16 @@
+HTMLSlotElement.prototype.assign || (
+	HTMLSlotElement.prototype.assign = {
+		assign(...nodes) {
+			for (let root = this.getRootNode(), i = 1; i < 256; ++i) {
+				if (root.querySelector('slot[name="s' + i + '"]')) continue
+				this.name = 's' + i
+				break
+			}
+			for (const node of nodes) {
+				if (node.slot !== undefined) {
+					node.slot = this.name
+				}
+			}
+		}
+	}.assign
+)
