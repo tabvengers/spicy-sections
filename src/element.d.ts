f85d6b77ad9281535e8cfa33eb42aff0a3cc8383
Jonathan Neal <jonathantneal@hotmail.com>
2022-03-16 10:54:13 -0400

    take three

--- src/element.d.ts
+++ src/element.d.ts
@@ -1,33 +1,129 @@
-export namespace Internals {
-	export type H = {
-		[K in keyof HTMLElementTagNameMap]: {
-			(props: Record<string, any>, ...children: any[]): K extends keyof HTMLElementTagNameMap ? HTMLElementTagNameMap[K] : HTMLElement
-			(...children: any[]): K extends keyof HTMLElementTagNameMap ? HTMLElementTagNameMap[K] : HTMLElement
-		}
-	} & {
-		[K in string]: {
-			(props: Record<string, any>, ...children: any[]): HTMLElement
-			(...children: any[]): HTMLElement
+// export namespace Internals {
+// 	export interface Internals {
+// 		[key: string]: any
+
+// 		entrySet: Link[]
+// 		entryMap: WeakMap<HTMLButtonElement, Link>
+
+// 		addEntry(): Link
+// 	}
+
+// 	export type H = {
+// 		[K in keyof HTMLElementTagNameMap]: {
+// 			(props: Record<string, any>, ...children: any[]): K extends keyof HTMLElementTagNameMap ? HTMLElementTagNameMap[K] : HTMLElement
+// 			(...children: any[]): K extends keyof HTMLElementTagNameMap ? HTMLElementTagNameMap[K] : HTMLElement
+// 		}
+// 	} & {
+// 		[K in string]: {
+// 			(props: Record<string, any>, ...children: any[]): HTMLElement
+// 			(...children: any[]): HTMLElement
+// 		}
+// 	}
+
+// 	export interface Link {
+// 		label: HTMLButtonElement
+// 		panel: HTMLDivElement
+// 		active: boolean
+// 		prev: Link | null
+// 		next: Link | null
+// 		all: Link[]
+// 	}
+
+// 	export interface Links {
+// 		all: Link[]
+
+// 		ref: WeakMap<EventTarget, Link>
+
+// 		clear(): void
+
+// 		add(label: Link['label'], panel: Link['panel']): void
+
+// 		get(target: EventTarget): Link
+
+// 		toggleOne(target: EventTarget & HTMLElement): void
+// 	}
+// }
+
+export type AffordanceType = 'collapse' | 'exclusive-collapse' | 'tab-bar'
+
+export interface Internals {
+	affordance: AffordanceType
+	sectionSet: Paneled.Section[],
+	sectionMap: WeakMap<HTMLButtonElement, Paneled.Section>
+
+	addSection(contentSection: Content.Section): Paneled.Section
+
+	toggle(paneledSection: Paneled.Section): void
+
+	templates: {
+		[K in AffordanceType]: {
+			(container: HTMLElement): {
+				addSection(section: Paneled.Section): void
+			}
 		}
 	}
 
-	export interface Link {
-		label: HTMLButtonElement
-		panel: HTMLDivElement
-		prev: Link | null
-		next: Link | null
-		all: Link[]
+	refresh(): void
+}
+
+export namespace Content {
+	export type Section = {
+		label: HTMLHeadingElement,
+		panel: ChildNode[]
 	}
 
-	export interface Links {
-		all: Link[]
-
-		ref: WeakMap<EventTarget, Link>
-
-		add(label: Link['label'], panel: Link['panel']): void
-
-		get(target: EventTarget): Link
-
-		toggleOne(target: EventTarget & HTMLElement): void
+	export interface GetSections {
+		(host: HTMLElement): Section[]
 	}
 }
+
+export namespace Paneled {
+	export interface SectionLabel {
+		element: HTMLButtonElement
+		marker: SVGSVGElement
+		slot: HTMLSlotElement
+		slotted: HTMLHeadingElement
+	}
+
+	export interface SectionPanel {
+		element: HTMLDivElement
+		slot: HTMLSlotElement
+		slotted: ChildNode[]
+	}
+
+	export interface Section {
+		label: Paneled.SectionLabel
+		panel: Paneled.SectionPanel
+		open: boolean
+		prev: Paneled.Section | null
+		next: Paneled.Section | null
+	}
+}
+
+export type ChildNode = Element | Text
+
+export type ElementName = keyof HTMLElementTagNameMap | keyof SVGElementTagNameMap | (string & Record<never, never>)
+
+export type AnyElement<T extends ElementName = ElementName> = T extends keyof HTMLElementTagNameMap ? HTMLElementTagNameMap[T] : T extends keyof SVGElementTagNameMap ? SVGElementTagNameMap[T] : HTMLElement
+
+export interface AnyNode extends Element, Text {}
+
+export type ElementProps<E extends Element> = {
+	[K in keyof E]: K[E]
+}
+
+export interface AssignElement<E extends Element = Element> {
+	(props: ElementProps<E>, ...children: any[]): E
+	(...children: any[]): E
+}
+
+type ElementProxy = {
+	[K in ElementName]: AssignElement<AnyElement<K>>
+}
+
+export interface CreateElement extends ElementProxy {
+	<T extends ElementName | AnyElement, E = T extends ElementName ? AnyElement<T> : T>(target: T, props: ElementProps<E>, ...children: any[]): E
+	<T extends ElementName | AnyElement, E = T extends ElementName ? AnyElement<T> : T>(target: T, ...children: any[]): E
+}
+
+export type AnyString = string & Record<never, never>
--- src/element.js
+++ src/element.js
@@ -8,16 +8,8 @@ import './polyfill.js'
 export class OUIPanelset extends HTMLElement {
 	#internals = createInternals(this)
 
-	connectedCallback() {
-		this.#internals.connectedCallback()
-	}
-
 	get affordance() {
 		return this.#internals.affordance
-	}
-
-	set affordance(value) {
-		this.#internals.affordance = value
 	}
 }
 
@@ -25,188 +17,214 @@ export class OUIPanelset extends HTMLElement {
 // -----------------------------------------------------------------------------
 
 const createInternals = (/** @type {HTMLElement} */ host) => {
+	/** Panelset Shadow Root */
 	const shadowRoot = host.attachShadow({ mode: 'open', slotAssignment: 'manual' })
 
-	shadowRoot.append(
-		h.style(
-			// default styles for all affordances
-			':where(div){display:contents}',
-			':where(button){all:unset;display:block;outline:revert}',
-			':where([part~="labels"]){display:flex;gap:1em}',
-			':where([part~="panel"]:not([part~="open"])){display:none}',
-			// default styles for collapse affordance
-			':where([part~="label"][part~="collapse"]){align-items:center;display:flex;gap:.25em}',
-			':where([part~="label"] > svg){height:.75em;width:.75em;transform:rotate(90deg)}',
-			':where([part~="label"][part~="open"] > svg){transform:rotate(180deg)}',
-			// default styles for tab-bar affordance
-			':where([part~="label"][part~="tab-bar"][part~="open"]) ::slotted(*){text-decoration:underline}'
-		)
+	/** Panelset Container */
+	const container = h.div({ part: 'container' })
+
+	/** Panelset Default Styles */
+	const defaultStyle = h.style(
+		// default styles for all affordances
+		':where(div){display:contents}',
+		':where(button){all:unset;outline:revert}',
+		':where(svg){display:none}',
+		':where([part~="labels"]){display:flex;gap:1em}',
+		':where([part~="panel"]:not([part~="open"])){display:none}',
+
+		// default styles for collapse affordance
+		':where([part~="collapse"][part~="label"]){align-items:center;display:flex;gap:.25em}',
+		':where([part~="collapse"][part~="marker"]){display:block;height:.75em;width:.75em;transform:rotate(90deg)}',
+		':where([part~="collapse"][part~="marker"][part~="open"]){transform:rotate(180deg)}',
+
+		// default styles for tab-bar affordance
+		':where([part~="label"][part~="tab-bar"][part~="open"]) ::slotted(*){text-decoration:underline}'
 	)
 
-	const links = createLinks()
+	shadowRoot.append(container, defaultStyle)
 
 	const onclick = (/** @type {HTMLElementEventMap['click'] & { currentTarget: HTMLButtonElement }} */ event) => {
-		const item = links.get(event.currentTarget)
-
-		// const isTabBar = item.label.part.contains('tab-bar')
-
-		links.toggleOne(event.currentTarget)
-
-		// const open = item.label.part.toggle('open')
-		
-		// item.panel.part.toggle('open', open)
-
-		// const attributeName = item.label.part.contains('tab-bar') ? 'aria-selected' : 'aria-expanded'
-
-		// item.label.setAttribute(attributeName, String(open))
+		internals.toggle(internals.sectionMap.get(event.currentTarget))
 	}
 
-	const onkeydown = (/** @type {HTMLElementEventMap['keydown']} */ event) => {
-		const item = links.get(event.currentTarget)
+	const onkeydown = (/** @type {HTMLElementEventMap['keydown'] & { currentTarget: HTMLButtonElement }} */ event) => {
+		const section = internals.sectionMap.get(event.currentTarget)
 
 		switch (event.key) {
 			case 'ArrowUp':
 			case 'ArrowLeft':
-				if (item.prev) {
-					item.prev.label.focus()
+				if (section.prev) {
+					section.prev.label.element.focus()
 				}
 				break
 			case 'ArrowDown':
 			case 'ArrowRight':
-				if (item.next) {
-					item.next.label.focus()
+				if (section.next) {
+					section.next.label.element.focus()
 				}
 				break
 		}
 	}
 
+	/** @type {Internals} */
 	const internals = {
 		affordance: 'tab-bar',
-		activeIndex: 1,
-		templates: {},
-		observer: new MutationObserver(
-			() => internals.refresh()
-		),
+		sectionSet: [],
+		sectionMap: new WeakMap(),
+		addSection(contentSection) {
+			/** Section Index. */
+			const index = internals.sectionSet.length
+
+			/** Section Label. */
+			const label = /** @type {PaneledSection['label']} */ ({
+				/** Label (`<button part="label">`). */
+				element: h.button({ part: 'label', id: 'label-' + index, ariaControls: 'panel-' + index, onclick, onkeydown }),
+
+				/** Marker (`<svg part="marker">`). */
+				marker: h.svg({ part: 'marker', viewBox: '0 0 270 240' }, h.polygon({ points: '5,235 135,10 265,235' })),
+
+				/** Slot (`<slot>`). */
+				slot: h.slot({ name: 'label-' + index }),
+
+				/** Slotted Element (`<h>`). */
+				slotted: contentSection.label,
+			})
+
+			/** Section Panel. */
+			const panel = /** @type {PaneledSection['panel']} */ ({
+				/** Section Panel (`<div part="panel">`). */
+				element: h.div({ part: 'panel', id: 'panel-' + index, ariaLabelledby: 'label-' + index }),
+
+				/** Slot (`<slot>`). */
+				slot: h.slot({ name: 'panel-' + index }),
+
+				/** Slotted Panel Elements. */
+				slotted: contentSection.panel,
+			})
+
+			// any previously added section
+			const prev = internals.sectionSet[internals.sectionSet.length - 1] || null
+
+			// create a new section (linked to any previous section)
+			const section = /** @type {PaneledSection} */ ({ label, panel, open: false, prev, next: null })
+	
+			// link the new section to any previous section
+			if (prev) prev.next = section
+	
+			// add the new section to the section set
+			internals.sectionSet.push(section)
+
+			// link the label element to the section
+			internals.sectionMap.set(label.element, section)
+
+			// append the label marker and label slot to the label element
+			label.element.append(label.marker, label.slot)
+
+			// append the panel slot to the panel element
+			panel.element.append(panel.slot)
+
+			return section
+		},
+		toggle(paneledSection) {
+			switch (internals.affordance) {
+				case 'collapse': {
+					const open = paneledSection.open = !paneledSection.open
+
+					paneledSection.label.element.part.toggle('open', open)
+					paneledSection.label.marker.part.toggle('open', open)
+					paneledSection.panel.element.part.toggle('open', open)
+
+					host.dispatchEvent(new Event('toggle', { bubbles: true, cancelable: false, composed: true }))
+
+					break
+				}
+
+				case 'tab-bar': {
+					if (paneledSection.open) return
+
+					for (const eachPaneledSection of internals.sectionSet) {
+						const open = eachPaneledSection === paneledSection
+
+						h(eachPaneledSection.label.element, { tabIndex: open ? 0 : -1 })
+
+						eachPaneledSection.label.element.part.toggle('open', open)
+						eachPaneledSection.label.marker.part.toggle('open', open)
+						eachPaneledSection.panel.element.part.toggle('open', open)
+					}
+
+					host.dispatchEvent(new Event('toggle', { bubbles: true, cancelable: false, composed: true }))
+
+					break
+				}
+			}
+		},
 		refresh() {
 			if (!host.isConnected) return
 
-			internals.content = getContent(...host.childNodes)
+			const template = internals.templates[internals.affordance](container)
 
-			const { affordance, createTemplate } = internals
+			for (const contentSection of getContentSections(host)) {
+				const paneledSection = internals.addSection(contentSection)
 
-			createTemplate[affordance]()
+				template.addSection(paneledSection)
+
+				paneledSection.label.slot.assign(paneledSection.label.slotted)
+				paneledSection.panel.slot.assign(...paneledSection.panel.slotted)
+			}
 		},
-		connectedCallback() {
-			internals.observer.observe(host, { childList: true })
+		templates: {
+			'collapse'(container) {
+				h(container, { part: 'container collapse' })
 
-			internals.refresh()
-		},
-		createTemplate: {
-			'collapse'() {
-				// <div part="container collapse">
-				const shadowContainer = h.div({ part: 'container collapse' })
+				return {
+					addSection(section) {
+						h(section.label.element, { part: 'label collapse', tabIndex: null, ariaSelected: 'false' })
+						h(section.label.marker, { part: 'marker collapse' })
+						h(section.panel.element, { part: 'panel collapse' })
 
-				shadowRoot.append(shadowContainer)
-
-				for (const [ index, actualLabel, ...actualPanels ] of internals.content) {
-					// <slot name="label">
-					const shadowLabelSlot = h.slot({ name: 'label' })
-
-					// <svg>
-					const shadowMarker = h.svg({ viewBox: '0 0 270 240' }, h.polygon({ points: '5,235 135,10 265,235' }))
-
-					// <button part="label collapse">
-					const shadowLabel = h.button({ part: 'label collapse', id: 'label-' + index, ariaControls: 'panel-' + index, ariaExpanded: false, onclick, onkeydown }, shadowMarker, shadowLabelSlot)
-
-					// <slot name="panel">
-					const shadowPanelSlot = h.slot({ name: 'panel' })
-
-					// <div part="panel collapse">
-					const shadowPanel = h.div({ part: 'panel collapse', id: 'panel-' + index, ariaLabelledby: 'label-' + index }, shadowPanelSlot)
-
-					links.add(shadowLabel, shadowPanel)
-
-					shadowContainer.append(shadowLabel)
-					shadowContainer.append(shadowPanel)
-
-					shadowLabelSlot.assign(actualLabel)
-					shadowPanelSlot.assign(...actualPanels)
-				}
-
-				return shadowContainer
-			},
-			'tab-bar'() {
-				// <div part="container tab-bar">
-				const shadowContainer = h.div({ part: 'container tab-bar' })
-
-				// <div part="labels tab-bar">
-				const labelsContainer = h.div({ part: 'labels tab-bar', role: 'tablist' })
-
-				// <div part="panels tab-bar">
-				const panelsContainer = h.div({ part: 'panels tab-bar' })
-
-				shadowContainer.append(labelsContainer, panelsContainer)
-
-				shadowRoot.append(shadowContainer)
-
-				for (const [ index, actualLabel, ...actualPanels ] of internals.content) {
-					// <slot name="label">
-					const shadowLabelSlot = h.slot({ name: 'label-' + index })
-
-					// <button part="label tab-bar">
-					const shadowLabel = h.button({ part: 'label tab-bar', role: 'tab', id: 'label-' + index, ariaControls: 'panel-' + index, ariaSelected: false, tabIndex: -1, onclick, onkeydown }, shadowLabelSlot)
-
-					// <slot name="panel">
-					const shadowPanelSlot = h.slot({ name: 'panel-' + index })
-
-					// <div part="panel tab-bar">
-					const shadowPanel = h.div({ part: 'panel tab-bar', role: 'tabpanel', id: 'panel-' + index, ariaLabelledby: 'label-' + index }, shadowPanelSlot)
-
-					links.add(shadowLabel, shadowPanel)
-
-					labelsContainer.append(shadowLabel)
-					panelsContainer.append(shadowPanel)
-
-					shadowLabelSlot.assign(actualLabel)
-					shadowPanelSlot.assign(...actualPanels)
-
-					if (index === internals.activeIndex) {
-						shadowLabel.tabIndex = 0
-						shadowLabel.part.add('open')
-						shadowPanel.part.add('open')
+						container.append(section.label.element, section.panel.element)
 					}
 				}
+			},
+			'exclusive-collapse'(container) {
+				container.part.value = 'container exclusive-collapse'
 
-				return shadowContainer
+				return {
+					addSection(section) {}
+				}
+			},
+			'tab-bar'(container) {
+				h(container, { part: 'container tab-bar' })
+
+				const labels = h.div({ part: 'labels tab-bar', role: 'tablist' })
+				const panels = h.div({ part: 'panels tab-bar' })
+
+				container.append(labels, panels)
+
+				return {
+					addSection(section) {
+						const open = internals.sectionSet.length === 1
+						const part = open ? ' open' : ''
+
+						h(section.label.element, { part: 'label tab-bar' + part, tabIndex: open ? 0 : -1, ariaSelected: String(!open) })
+						h(section.label.marker, { part: 'marker tab-bar' + part })
+						h(section.panel.element, { part: 'panel tab-bar' + part })
+
+						labels.append(section.label.element)
+						panels.append(section.panel.element)
+					}
+				}
 			},
 		},
 	}
 
+	const observer = new MutationObserver(internals.refresh)
+
+	observer.observe(host, { childList: true })
+
+	internals.refresh()
+
 	return internals
-}
-
-const getContent = (/** @type {ChildNode[]} */ ...childNodes) => {
-	/** @type {[ number, HTMLHeadingElement, ...(Element | Text)[] ][]} */
-	const panelset = []
-
-	/** @type {[ number, HTMLHeadingElement, ...Node[] ]} */
-	let panel = /** @type {any} */ ([])
-
-	let index = 0
-
-	for (const childNode of childNodes) {
-		if (childNode instanceof Element) {
-			childNode.removeAttribute('slot')
-		}
-
-		if (childNode instanceof HTMLHeadingElement) {
-			panelset.push(panel = [ ++index, childNode ])
-		} else {
-			panel.push(childNode)
-		}
-	}
-
-	return panelset
 }
 
 // utilities
@@ -214,65 +232,66 @@ const getContent = (/** @type {ChildNode[]} */ ...childNodes) => {
 
 const toDashedCase = (/** @type {string} */ value) => value.replace(/[A-Z]/g, '-$&').toLowerCase()
 
-const h = new Proxy(/** @type {import('./element').Internals.H} */ ({}), {
-	get(_, /** @type {string} */ name) {
-		const isSVG = name === 'svg' || name === 'polygon'
-		const element = isSVG ? document.createElementNS('http://www.w3.org/2000/svg', name) : document.createElement(name)
+const isWritableProp = (/** @type {any} */ value) => value === null || typeof value !== 'object'
 
-		return (/** @type {any[]} */ ...args) => {
-			const props = typeof args[0] === 'object' && Object(args[0]).constructor === Object ? args.shift() : null
+const h = new Proxy(/** @type {CreateElement} */ ((/** @type {AnyElement | ElementName} */ target, /** @type {any} */ ...args) => {
+	const element = typeof target === 'string' ? target === 'svg' || target === 'polygon' ? document.createElementNS('http://www.w3.org/2000/svg', target) : document.createElement(target) : target
 
-			for (const prop in props) {
-				if (prop in element && (element[prop] === null || typeof element[prop] === 'string')) element[prop] = props[prop]
-				else element.setAttribute(isSVG ? prop : toDashedCase(prop), props[prop])
-			}
+	const isSVG = element instanceof SVGElement
 
-			element.append(...args)
+	const props = typeof args[0] === 'object' && Object(args[0]).constructor === Object ? args.shift() : null
 
-			return element
+	for (const prop in props) {
+		if (prop in element && isWritableProp(element[prop])) {
+			element[prop] = props[prop]
+		} else {
+			element.setAttribute(isSVG ? prop : toDashedCase(prop), props[prop])
 		}
+	}
+
+	element.append(...args)
+
+	return element
+}), {
+	get(_, /** @type {string} */ name) {
+		return (/** @type {any} */ ...args) => h(name, ...args)
 	}
 })
 
-/** @type {() => import('./element').Internals.Links} */ 
-const createLinks = () => ({
-	all: [],
-	ref: new WeakMap,
-	add(label, panel) {
-		const { all, ref } = this
-		const prev = all[all.length - 1] || null
+/** Returns sections of content extracted from `<oui-panelset>`. */
+const getContentSections = (/** @type {HTMLElement} */ host) => {
+	/** All child nodes of the panelset element. */
+	let nodes = /** @type {ChildNode[]} */ (/** @type {any} */ (host.childNodes))
 
-		/** @type {import('./element').Internals.Link} */
-		const link = { label, panel, prev, next: null, all }
+	/** All sections extracted from the panelset element. */
+	let sections = /** @type {ContentSection[]} */ ([])
 
-		if (prev) prev.next = link
+	/** Any section extracted from the panelset element. */
+	let section = /** @type {ContentSection} */ ({ label: null, panel: [] })
 
-		all.push(link)
-		ref.set(label, link)
-	},
-	get(target) {
-		return this.ref.get(target)
-	},
-	toggleOne(target) {
-		if (target.part.contains('open')) return
-
-		const isTabBar = target.part.contains('tab-bar')
-		const { all, ref } = this
-		const link = ref.get(target)
-		const attributeName = isTabBar ? 'aria-selected' : 'aria-expanded'
-
-		for (const anyLink of all) {
-			if (anyLink === link) {
-				anyLink.label.tabIndex = 0
-				anyLink.label.setAttribute(attributeName, 'true')
-				anyLink.label.part.add('open')
-				anyLink.panel.part.add('open')
-			} else {
-				anyLink.label.tabIndex = -1
-				anyLink.label.setAttribute(attributeName, 'false')
-				anyLink.label.part.remove('open')
-				anyLink.panel.part.remove('open')
-			}
+	for (const node of nodes) {
+		if (node instanceof HTMLHeadingElement) {
+			// add a new section, if the child node is a heading
+			sections.push(section = { label: node, panel: [] })
+		} else {
+			// otherwise, append the child node to the existing section
+			section.panel.push(node)
 		}
-	},
-})
+
+		// remove any existing slotting
+		if (node instanceof Element) {
+			node.removeAttribute('slot')
+		}
+	}
+
+	return sections
+}
+
+/** @typedef {import('./element').ChildNode} ChildNode */
+/** @typedef {import('./element').ElementName} ElementName */
+/** @typedef {import('./element').AnyElement} AnyElement */
+/** @typedef {import('./element').AnyNode} AnyNode */
+/** @typedef {import('./element').CreateElement} CreateElement */
+/** @typedef {import('./element').Internals} Internals */
+/** @typedef {import('./element').Paneled.Section} PaneledSection */
+/** @typedef {import('./element').Content.Section} ContentSection */
